53e4182234dca65f2a2ff0c2f624abb4a4c194c8
Derrick Camerino <robustdj@gmail.com>
2018-12-16 18:30:57 -0800

    Updates

---
+++ precache-manifest.69fc2dcb42446d3f1097efd6bd6186f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "d63d384ade6a6cd1b694",
+    "url": "/contactmanager/static/js/main.d63d384a.chunk.js"
+  },
+  {
+    "revision": "bdf86645ffaebba6b663",
+    "url": "/contactmanager/static/js/1.bdf86645.chunk.js"
+  },
+  {
+    "revision": "d63d384ade6a6cd1b694",
+    "url": "/contactmanager/static/css/main.da3b6268.chunk.css"
+  },
+  {
+    "revision": "bdf86645ffaebba6b663",
+    "url": "/contactmanager/static/css/1.7bdb55e1.chunk.css"
+  },
+  {
+    "revision": "982408956d92a9333a83557f79402367",
+    "url": "/contactmanager/index.html"
+  }
+];
